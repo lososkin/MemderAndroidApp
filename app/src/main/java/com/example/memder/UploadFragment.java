3fffc0059f88630b2e0f55a610b1a0c720c23b8a
Losos <yalososka@gmail.com>
2019-10-14 02:55:24 +0300

    Uploader fragment finished

--- app/src/main/java/com/example/memder/UploadFragment.java
+++ app/src/main/java/com/example/memder/UploadFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,6 +24,8 @@ import androidx.fragment.app.Fragment;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -34,6 +37,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class UploadFragment extends Fragment {
     View view ;
+    File imageFile ;
     private static int RESULT_LOAD_IMAGE = 1;
 
     private String getRealPathFromURI(Uri contentURI) {
@@ -50,40 +54,9 @@ public class UploadFragment extends Fragment {
         return result;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
-//            if (ContextCompat.checkSelfPermission(MenuActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
-//                    != PackageManager.PERMISSION_GRANTED) {
-        String[] PERMISSIONS_STORAGE = {
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-        ActivityCompat.requestPermissions(getActivity(), PERMISSIONS_STORAGE, 1);
-
-        final Uri selectedImageURI = data.getData();
-        final File imageFile = new File(getRealPathFromURI(selectedImageURI));
-
-        ImageView imageView = (ImageView) view.findViewById(R.id.UploadedImage);
-//        imageView.setImageURI(selectedImageURI);
-
-        final int size = (int) imageFile.length();
-        byte[] bytes = new byte[size];
-        try {
-            BufferedInputStream buf = new BufferedInputStream(new FileInputStream(imageFile));
-            buf.read(bytes, 0, bytes.length);
-            buf.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        final String Str_image = Base64.encodeToString(bytes, Base64.DEFAULT);
-
+    private void uploadImage(File imageFile){
+        final TextView statusView =  (TextView) view.findViewById(R.id.status);
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
         SharedPreferences prefs = this.getActivity().getSharedPreferences("token", Context.MODE_PRIVATE);
@@ -101,85 +74,68 @@ public class UploadFragment extends Fragment {
             @Override
             public void onStart() {
                 // called before request is started
+                statusView.setText("Загружается");
             }
-
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                 // called when response HTTP status is "200 OK"
                 System.out.println("OK-----");
+                statusView.setText("Загружено");
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
                 if (errorResponse != null) {
                     System.out.println(new String(errorResponse));
+                    statusView.setText("Не удалось загрузить");
                 }
             }
 
             @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
+            public void onFinish() {
+                // called before request is finish
             }
+
         });
-
-//                final MediaType MEDIA_TYPE_JPEG = MediaType.parse("image/jpeg");
-//                AsyncTask.execute(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        SharedPreferences prefs = getSharedPreferences("token", MODE_PRIVATE);
-//                        String tokenFromStorage = prefs.getString("token", "Token not found");
-////                        AsyncHttpClient client = new AsyncHttpClient();
-////                        RequestParams params = new RequestParams();
-////                        try {
-////                            params.put("profile_picture", imageFile);
-////                            params.put("text", "Мем из андроида");
-////                        } catch(FileNotFoundException e) {}
-////
-////                        client.addHeader("Authorization", "Token " + tokenFromStorage);
-//
-//
-////                        OkHttpClient client = new OkHttpClient();
-////                        SharedPreferences prefs = getSharedPreferences("token", MODE_PRIVATE);
-////                        String tokenFromStorage = prefs.getString("token", "Token not found");
-////                        RequestBody requestFile = RequestBody.create(MediaType.parse("image/jpeg"), getRealPathFromURI(selectedImageURI));
-////                        RequestBody requestBody = new MultipartBody.Builder()
-//////                                .setType(MultipartBody.FORM)
-////                                .addFormDataPart("img", Str_image)
-////                                .addFormDataPart("text", "adsadg")
-////                                .build();
-//////                        RequestBody body = new MultipartBody.Builder()
-//////                                .addFormDataPart("img",imageFile.getName(),requestFile)
-//////                                .addFormDataPart("text", "dhashkd")
-//////                                .build();
-////                        Request request = new Request.Builder()
-////                                .url("http://memnderapi.pythonanywhere.com/memes/api/create/")
-////                                .header("Authorization", "Token " + tokenFromStorage)
-////                                .post(requestBody)
-////                                .build();
-////                        try {
-////                            Response response = client.newCall(request).execute();
-////                            System.out.println(response.code());
-//////                            JSONObject json = new JSONObject(response.body().toString());
-//////                            Iterator<?> keys = json.keys();
-//////                            while( keys.hasNext() ) {
-//////                                String key = (String) keys.next();
-//////                                System.out.println(key);
-//////                                System.out.println(json.get(key));
-//////                            }
-////                        } catch (IOException e) {
-////                            e.printStackTrace();
-////                        }
-//                    }
-//                });
-
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+//            if (ContextCompat.checkSelfPermission(MenuActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+//                    != PackageManager.PERMISSION_GRANTED) {
+        String[] PERMISSIONS_STORAGE = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        ActivityCompat.requestPermissions(getActivity(), PERMISSIONS_STORAGE, 1);
+
+        final Uri selectedImageURI = data.getData();
+        imageFile = new File(getRealPathFromURI(selectedImageURI));
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.previewImage);
+        imageView.setImageURI(selectedImageURI);
+        TextView statusView = (TextView) view.findViewById(R.id.status);
+        statusView.setText("Теперь нажмите кнопку загрузки");
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_upload,container,false);
         Button mUpload = (Button) view.findViewById(R.id.UploadImage);
         mUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage(imageFile);
+            }
+        });
+
+        Button mChoose = (Button) view.findViewById(R.id.chooseMem);
+        mChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(
@@ -189,6 +145,7 @@ public class UploadFragment extends Fragment {
                 startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
+
         return view;
     }
 }
