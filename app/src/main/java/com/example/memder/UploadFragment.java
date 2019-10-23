835d93921ac75cc40d8ff01cb1578cbdf0ced77e
Losos <yalososka@gmail.com>
2019-10-23 21:23:22 +0300

    fix image orientation and resize image before upload on server

--- app/src/main/java/com/example/memder/UploadFragment.java
+++ app/src/main/java/com/example/memder/UploadFragment.java
@@ -5,8 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.LayoutInflater;
@@ -21,6 +26,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -28,12 +34,19 @@ import com.loopj.android.http.RequestParams;
 import org.w3c.dom.Text;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.mime.content.ByteArrayBody;
+
+import static com.bumptech.glide.load.resource.bitmap.TransformationUtils.rotateImage;
 
 public class UploadFragment extends Fragment {
     View view ;
@@ -54,20 +67,46 @@ public class UploadFragment extends Fragment {
         return result;
     }
 
+    private Bitmap fixOrientation(Bitmap bitmap,String path) {
+        ExifInterface ei = null;
+        Bitmap selectedBitmap;
+        try {
+            ei = new ExifInterface(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
 
-    private void uploadImage(File imageFile){
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                selectedBitmap = rotateImage(bitmap, 90);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                selectedBitmap = rotateImage(bitmap, 180);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                selectedBitmap = rotateImage(bitmap, 270);
+                break;
+            case ExifInterface.ORIENTATION_NORMAL:
+                selectedBitmap = bitmap;
+                break;
+            default:
+                selectedBitmap = bitmap;
+        }
+        return selectedBitmap;
+    }
+
+    private void uploadImage(File imageFile) throws FileNotFoundException {
         final TextView statusView =  (TextView) view.findViewById(R.id.status);
         AsyncHttpClient client = new AsyncHttpClient();
         RequestParams params = new RequestParams();
         SharedPreferences prefs = this.getActivity().getSharedPreferences("token", Context.MODE_PRIVATE);
         String tokenFromStorage = prefs.getString("token", "Token not found");
         client.addHeader("Authorization", "Token " + tokenFromStorage);
-        try {
-            params.put("img", imageFile);
-            System.out.println("image in params");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+
+        params.put("img",imageFile);
+        System.out.println("image in params");
+
         params.put("text", "Mem from android");
         client.post(Settings.host+"/memes/api/create/", params,new AsyncHttpResponseHandler() {
 
@@ -115,14 +154,44 @@ public class UploadFragment extends Fragment {
 
 
             final Uri selectedImageURI = data.getData();
-            imageFile = new File(getRealPathFromURI(selectedImageURI));
 
+            Bitmap bitmap = BitmapFactory.decodeFile(getRealPathFromURI(selectedImageURI));
+            bitmap = fixOrientation(bitmap,getRealPathFromURI(selectedImageURI));
+            int maxHeight = 500;
+            int maxWidth = 500;
+            float scale = Math.min(((float)maxHeight / bitmap.getWidth()), ((float)maxWidth / bitmap.getHeight()));
 
+            Matrix matrix = new Matrix();
+            matrix.postScale(scale, scale);
+
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 
             ImageView imageView = (ImageView) view.findViewById(R.id.previewImage);
             System.out.println(selectedImageURI);
 
-            imageView.setImageURI(selectedImageURI);
+            imageView.setImageBitmap(bitmap);
+
+
+            try {
+                imageFile = new File(getRealPathFromURI(selectedImageURI));
+                File direct = new File(Environment.getExternalStorageDirectory()+"/Memder");
+
+                if(!direct.exists()) {
+                    if(direct.mkdir()); //directory is created;
+                }
+                OutputStream os = new BufferedOutputStream(new FileOutputStream(Environment.getExternalStorageDirectory()+"/Memder/"+imageFile.getName()));
+                System.out.println(getRealPathFromURI(selectedImageURI));
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+                os.close();
+                imageFile = new File(Environment.getExternalStorageDirectory()+"/Memder/"+imageFile.getName());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+//            Glide.with(getContext()).load("file:" + getRealPathFromURI(selectedImageURI)).into(imageView);
             TextView statusView = (TextView) view.findViewById(R.id.status);
             statusView.setText("Теперь нажмите кнопку загрузки");
         }
@@ -136,7 +205,11 @@ public class UploadFragment extends Fragment {
         mUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImage(imageFile);
+                try {
+                    uploadImage(imageFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
